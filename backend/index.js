8e216163d90a20ce53e6eb394702a987f930392f
John Anjelo Basay <2124824@cmontmorency.qc.ca>
2025-09-11 22:41:08 -0400

    [Init] Initialization de React -> Frontend (npx create-react-app@latest frontend)

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const mysql = require("mysql2");
-const cors = require("cors");
-require("dotenv").config();
+const express = require("express"); // RESTful API
+const mysql = require("mysql2"); //pour connecter au mysql databases
+const cors = require("cors"); // autroriser le frontend sur localhost:3000 a communiquer avec le backend sur localhost:3001
+require("dotenv").config(); //.env = pr que les infos sensibles de la database soient stockees en dehors du code
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); //traducteur: le frontend comm en JSON, cekui-ci le traduit en un fformat que Node/Exoress peut comprendre
 
 //connexion avec le database
 const db = mysql.createConnection({
