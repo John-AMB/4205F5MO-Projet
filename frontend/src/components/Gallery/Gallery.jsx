38cc0c9eae4b89cf426bb1eabf0c74c50943d201
Sean <seanskyjune@gmail.com>
2025-09-17 22:06:12 -0400

    [Add] gallery pour les images/idees

--- frontend/src/components/Gallery/Gallery.jsx
+++ frontend/src/components/Gallery/Gallery.jsx
@@ -1,2 +1,16 @@
-const Gallery = () => {};
+import ideas from "../../../public/ideas.json";
+
+const Gallery = () => {
+  return (
+    <div className="gallery">
+      {ideas.map((item, index) => (
+        <div key={index} className="galleryItem">
+          <img src={item.photo} alt={item.title} />
+          <p>{item.title}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
 export default Gallery;
--- frontend/src/components/GalleyItem/GalleryItem.jsx
+++ frontend/src/components/GalleyItem/GalleryItem.jsx
@@ -1,14 +1,34 @@
-const GalleryItem = () => {
+import { useState, useRef } from "react";
+import "./GalleryItem.css";
+
+const GalleryItem = ({ item }) => {
+  const [open, setOpen] = useState(false);
+  const buttonRef = useRef();
+
+  const toggleOptions = () => {
+    setOpen((prev) => !prev);
+  };
+
   return (
-    <div className="galleryItem" style={{ gridRowEnd: `span ${span}` }}>
-      <img ref={ref} src={item.photo} alt={item.title} />
+    <div className="galleryItem">
+      <img src={item.photo} alt={item.title} />
 
       <div className="overlayIcons">
-        <button ref={}>
+        <button ref={buttonRef} onClick={toggleOptions}>
           <img src="/general/more.svg" alt="options" />
         </button>
       </div>
+
+      {open && (
+        <div className="userOptions">
+          <div className="optionFormat">Edit</div>
+          <div className="optionFormat">Delete</div>
+        </div>
+      )}
+
+      <p>{item.title}</p>
     </div>
   );
 };
+
 export default GalleryItem;
